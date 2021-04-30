ba2f1dd2b36dd5c4d4c2829c913d85207fd9cfde
Adi Ravishankara <adiravishankara@gmail.com>
2021-04-30 13:01:58 -0700

    compnent testing code

--- src/UI_v2/component_testing_UI.py
+++ src/UI_v2/component_testing_UI.py
@@ -117,7 +117,10 @@ class GUI(QWidget):
 			self.buttonSetup()
 		except:
 			print("Error with Button Setup")
-		# self.graphSetup()
+		try:
+			self.graphSetup()
+		except:
+			print("Error with Graph Setup")
 		try:
 			self.UI()
 		except:
@@ -125,17 +128,18 @@ class GUI(QWidget):
 
 	def systemSetup(self):
 		self.kit = MotorKit(i2c=board.I2C())
-		# self.adc = adc.ADS1115(0x48)
+		self.adc = adc.ADS1115(0x48)
 		self.SM = self.Stepper(self.kit.stepper1)
 		self.valve = self.MOTOR(self.kit.motor3, "Valve")
 		self.pump = self.MOTOR(self.kit.motor4, "Pump")
 
 		self.valve.deactivate()
 		self.pump.deactivate()
+		self.SM.motor.release()
 
-		# self.sensor1 = self.MOS(self.adc, 0)
-		# self.sensor2 = self.MOS(self.adc, 1)
-		# self.sensor3 = self.MOS(self.adc, 2)
+		self.sensor1 = self.MOS(self.adc, 0)
+		self.sensor2 = self.MOS(self.adc, 1)
+		self.sensor3 = self.MOS(self.adc, 2)
 
 	def buttonSetup(self):
 		self.b1 = self.button()
@@ -162,39 +166,39 @@ class GUI(QWidget):
 		self.b6.setButtonText("Toggle Pump")
 		self.b6.clicked.connect(lambda: self.pump.toggle())
 
-	# def graphSetup(self):
-	# 	self.sensorGraph = self.graph()
-	# 	self.sensorGraph.setYRange(0, 5)
-	#
-	# 	self.timeArray = list(range(200))
-	# 	self.sensor1Array = [0 for _ in range(200)]
-	# 	self.sensor2Array = [0 for _ in range(200)]
-	# 	self.sensor3Array = [0 for _ in range(200)]
-	#
-	# 	self.sensor1Plot = self.sensorGraph.plot(self.timeArray, self.sensor1Array, pen='r')
-	# 	self.sensor2Plot = self.sensorGraph.plot(self.timeArray, self.sensor2Array, pen='g')
-	# 	self.sensor2Plot = self.sensorGraph.plot(self.timeArray, self.sensor3Array, pen='b')
-	#
-	# 	def updateGraph():
-	# 		self.sensor1Array = self.sensor1Array[1:]
-	# 		self.sensor2Array = self.sensor2Array[1:]
-	# 		self.sensor3Array = self.sensor3Array[1:]
-	# 		self.sensor1Array.append(self.sensor1.read())
-	# 		self.sensor2Array.append(self.sensor2.read())
-	# 		self.sensor3Array.append(self.sensor3.read())
-	#
-	# 		self.sensor1Plot.setData(self.timeArray, self.sensor1Array)
-	# 		self.sensor2Plot.setData(self.timeArray, self.sensor2Array)
-	# 		self.sensor3Plot.setData(self.timeArray, self.sensor3Array)
-	#
-	# 	self.graphTimer = QTimer()
-	# 	self.graphTimer.timeout.connect(lambda: updateGraph())
-	# 	self.graphTimer.start(100)
+	def graphSetup(self):
+		self.sensorGraph = self.graph()
+		self.sensorGraph.setYRange(0, 5)
+
+		self.timeArray = list(range(200))
+		self.sensor1Array = [0 for _ in range(200)]
+		self.sensor2Array = [0 for _ in range(200)]
+		self.sensor3Array = [0 for _ in range(200)]
+
+		self.sensor1Plot = self.sensorGraph.plot(self.timeArray, self.sensor1Array, pen='r')
+		self.sensor2Plot = self.sensorGraph.plot(self.timeArray, self.sensor2Array, pen='g')
+		self.sensor2Plot = self.sensorGraph.plot(self.timeArray, self.sensor3Array, pen='b')
+
+		def updateGraph():
+			self.sensor1Array = self.sensor1Array[1:]
+			self.sensor2Array = self.sensor2Array[1:]
+			self.sensor3Array = self.sensor3Array[1:]
+			self.sensor1Array.append(self.sensor1.read())
+			self.sensor2Array.append(self.sensor2.read())
+			self.sensor3Array.append(self.sensor3.read())
+
+			self.sensor1Plot.setData(self.timeArray, self.sensor1Array)
+			self.sensor2Plot.setData(self.timeArray, self.sensor2Array)
+			self.sensor3Plot.setData(self.timeArray, self.sensor3Array)
+
+		self.graphTimer = QTimer()
+		self.graphTimer.timeout.connect(lambda: updateGraph())
+		self.graphTimer.start(100)
 
 	def UI(self):
 		self.layout = QGridLayout()
 
-		# self.layout.addWidget(self.sensorGraph, 0, 0, 5, 5)
+		self.layout.addWidget(self.sensorGraph, 0, 0, 5, 5)
 		self.layout.addWidget(self.b1, 6, 0, 1, 1)
 		self.layout.addWidget(self.b2, 6, 1, 1, 1)
 		self.layout.addWidget(self.b3, 7, 0, 1, 1)
