53840b45de71d6f3a6ee84db30e45d43088adade
Adi Ravishankara <adiravishankara@gmail.com>
2021-09-16 10:56:39 -0700

    changed pump settings

--- src/dev4/UI.py
+++ src/dev4/UI.py
@@ -451,10 +451,10 @@ class testWindow(baseWindow):
 		self.path = os.getcwd()
 		self.dataPath = "{}/data/".format(self.path)
 
-		self.sampleCollectTime = 30000  # normally 30000
-		self.exposeTime = 10000  # normally 10000
-		self.recoverTime = 50000  # normally 50000
-		self.endTestTime = 200000  # normally 120000
+		self.sampleCollectTime = 3000  # normally 30000
+		self.exposeTime = 1000  # normally 10000
+		self.recoverTime = 5000  # normally 50000
+		self.endTestTime = 20000  # normally 200000
 
 		self.sampleCollectTimer = QTimer()
 		self.sampleCollectTimer.setSingleShot(True)
@@ -497,9 +497,9 @@ class testWindow(baseWindow):
 
 	def startDataCollection(self):
 		# STEP 2: Purge Done, Starting test
-		self.SM.recover()
 		self.pump.deactivate()
 		self.valve.deactivate()
+		self.SM.recover()
 		self.testTimer.start(self.endTestTime)
 		# self.exposeTimer.start(self.exposeTime)
 		# self.recoveryTimer.start(self.recoverTime)
