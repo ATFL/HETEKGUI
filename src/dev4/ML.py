b04424628ef90e23003d59bbb7102b5405e3692d
Adi Ravishankara <adiravishankara@gmail.com>
2021-09-16 14:22:07 -0700

    trying machine learning

--- src/dev4/ML.py
+++ src/dev4/ML.py
@@ -1,7 +1,15 @@
 import os
+import sys
 from pathlib import Path
 import pickle5 as pickle
 import pandas as pd
+
+from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+
+app = QApplication(sys.argv)
 
 class ML:
 	def __init__(self, file):
@@ -12,14 +20,24 @@ class ML:
 		self.clfFile = open('classifier.obj', 'rb')
 		self.clf = pickle.load(self.clfFile)
 		self.clfVal = self.clf.predict(self.myarray)
-		print(self.clfVal)
-		return(self.clfVal)
+		# print(self.clfVal)
 
 	def loadFile(self, filename):
 		data = pd.read_csv("{}".format(filename), delimiter=',')
 		self.myarray = data.values
 		self.myarray = self.myarray[1, 0:3900].reshape(1, -1)
 		self.analyze()
+
+
+class display(QWidget):
+	def __init__(self, value):
+		super(display, self).__init__()
+		self.myLabel = QLabel()
+		self.myLabel.setText(str(value))
+		self.layout = QGridLayout()
+		self.layout.addWidget(self.myLabel)
+		self.setLayout(self.layout)
+		self.show()
 
 
 if __name__ == "__main__":
