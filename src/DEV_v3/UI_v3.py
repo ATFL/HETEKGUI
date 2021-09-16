331d6cd2e5a6a4bb37d7f6c336e02717e26d89ab
Adi Ravishankara <adiravishankara@gmail.com>
2021-09-16 12:06:28 -0700

    changed pump settings

--- src/DEV_v3/UI_v3.py
+++ src/DEV_v3/UI_v3.py
@@ -497,9 +497,10 @@ class testWindow(baseWindow):
 
 	def startDataCollection(self):
 		# STEP 2: Purge Done, Starting test
-		self.SM.recover()
+
 		self.pump.deactivate()
 		self.valve.deactivate()
+		self.SM.recover()
 		self.testTimer.start(self.endTestTime)
 		# self.exposeTimer.start(self.exposeTime)
 		# self.recoveryTimer.start(self.recoverTime)
